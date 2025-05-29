f429fb928e259109c6494c9d761ae3ac28ffa8b7
Mesh <sangmeshack9@gmail.com>
2025-05-29 17:01:02 +1000

    Column and Row changes

--- app/src/main/java/com/chemasas06/safari/MainActivity.kt
+++ app/src/main/java/com/chemasas06/safari/MainActivity.kt
@@ -4,14 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.chemasas06.safari.ui.theme.SafariTheme
+import androidx.compose.ui.graphics.Color
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +28,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             SafariTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
+                    TestLayout(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
@@ -31,17 +39,20 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TestLayout(name: String, modifier: Modifier = Modifier) {
+    Row(
+        modifier= modifier. fillMaxWidth()
+    ) {
+        Column (modifier. fillMaxWidth().width(100.dp).background(Color.Yellow)) {}
+        Column (modifier. fillMaxWidth().width(100.dp).background(Color.Green)){}
+        Column (modifier. fillMaxWidth().width(100.dp).background(Color.Red)) {}
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     SafariTheme {
-        Greeting("Android")
+        TestLayout("Android")
     }
 }
